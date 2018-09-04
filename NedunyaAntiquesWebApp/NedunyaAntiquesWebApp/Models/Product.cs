df288b43f59d1af9b8a400bd9b2ae18c5b4cfefb
Zach Bachar <zachbachar@gmail.com>
2018-09-04 13:47:50 +0300

    added product model with properties.

---
+++ NedunyaAntiquesWebApp/NedunyaAntiquesWebApp/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NedunyaAntiquesWebApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Substance { get; set; }
+        public String Category { get; set; }
+        public double Height { get; set; }
+    }
+}
