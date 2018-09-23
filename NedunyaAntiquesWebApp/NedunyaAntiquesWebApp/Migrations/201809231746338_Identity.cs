f5cd57182bbaa93408658e5361eba6408c0ed810
דניאל סגל <dannysegal1@gmail.com>
2018-09-23 21:37:56 +0300

    The only method the cuases problem in saving the new customer to the Db is the save. until there the customer is beeing creating

---
+++ NedunyaAntiquesWebApp/NedunyaAntiquesWebApp/Migrations/201809231746338_Identity.cs
@@ -0,0 +1,26 @@
+namespace NedunyaAntiquesWebApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Identity : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Transactions", "CustomerEmail", "dbo.Customers");
+            DropPrimaryKey("dbo.Customers");
+            AlterColumn("dbo.Customers", "Email", c => c.String(nullable: false, maxLength: 128));
+            AddPrimaryKey("dbo.Customers", "Email");
+            AddForeignKey("dbo.Transactions", "CustomerEmail", "dbo.Customers", "Email");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Transactions", "CustomerEmail", "dbo.Customers");
+            DropPrimaryKey("dbo.Customers");
+            AlterColumn("dbo.Customers", "Email", c => c.String(nullable: false, maxLength: 128));
+            AddPrimaryKey("dbo.Customers", "Email");
+            AddForeignKey("dbo.Transactions", "CustomerEmail", "dbo.Customers", "Email");
+        }
+    }
+}
