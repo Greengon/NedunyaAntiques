4b15f6f577269ddae64f4c3a14d635df9e795f3c
Gon Lubel <greengon1991@gmail.com>
2018-11-03 22:57:35 +0200

    Added some paypal code to transaction controller and view.

---
+++ NedunyaAntiquesWebApp/NedunyaAntiquesWebApp/ViewModels/TransactionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using NedunyaAntiquesWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NedunyaAntiquesWebApp.ViewModels
+{
+    public class TransactionViewModel
+    {
+        public List<Product> CartItems { get; set; }
+        public decimal amount { get; set; }
+    }
+}
